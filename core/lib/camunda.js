06672febd043b5068c438db5b848be318b889a5f
Volker Buzek <volker.buzek@camunda.com>
2024-09-24 16:48:48 +0200

    feat: add camunda connectivity

---
+++ core/lib/camunda.js
@@ -0,0 +1,61 @@
+const cds = require("@sap/cds")
+const LOGGER = cds.log("camunda")
+const { Camunda8 } = require("@camunda8/sdk")
+
+const DEBUG = cds.log("camunda")._debug || process.env.DEBUG?.includes("camunda")
+
+module.exports = Object.assign(
+  {},
+  {
+    /**
+     * @type {import("@camunda8/sdk").Camunda8}
+     */
+    _c8: null,
+    /**
+     * @type {import("@camunda8/sdk").Zeebe.ZeebeGrpcClient}
+     */
+    zeebe: null,
+    /**
+     * @type {import("@camunda8/sdk").Zeebe.ZeebeRestClient}
+     */
+    zeebeRest: null,
+    /**
+     * @type {import("@camunda8/sdk").Tasklist.TasklistApiClient}
+     */
+    tl: null,
+
+    init() {
+      if (!this.zeebe) {
+        LOGGER.info("init'ing camunda client...")
+        const options = {
+          onReady: () => {
+            LOGGER.info("zeebe grpc client connected!")
+          },
+          onConnectionError: () => LOGGER.info("zeebe grpc client disconnected...")
+        }
+        this._c8 = new Camunda8(options)
+        this.zeebe = this._c8.getZeebeGrpcApiClient(options)
+        this.zeebe.onReady = options.onReady
+        this.zeebeRest = this._c8.getZeebeRestClient()
+        this.tl = this._c8.getTasklistApiClient()
+        DEBUG && this.zeebe.topology().then((topology) => {
+          LOGGER.debug(JSON.stringify(topology, null, 2))
+        })
+      }
+    },
+
+    /**
+     * get the instance of one of the camunda clients
+     *
+     * @param {"zeebe"|"zeebeRest"|"tl"} which client to return
+     * @returns
+     */
+    getClient(which = "zeebe") {
+      if (!this[which]) {
+        LOGGER.info("no camunda client yet -> init'ing...")
+        this.init()
+      }
+      return this[which]
+    }
+  }
+)
--- core/srv/server.js
+++ core/srv/server.js
@@ -1,6 +1,14 @@
-// connect to Camunda Saas
+process.env.DEBUG?.includes("camunda") && require("debug").enable("camunda:*")
 
 const cds = require("@sap/cds")
 const LOGGER = cds.log("camunda")
+const camunda = require("../lib/camunda")
 
 LOGGER.info("started!")
+
+cds.on("served", async () => !process.env.DISABLE_CAMUNDA && camunda.init())
+cds.on("listening", function () {
+  // ...
+})
+
+module.exports = cds.server
