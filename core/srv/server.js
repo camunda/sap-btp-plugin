5dab82556fe18e156b2d9f483b91a7ec69f217df
Volker Buzek <volker.buzek@camunda.com>
2024-09-24 17:17:12 +0200

    feat: include in core

--- core/srv/server.js
+++ core/srv/server.js
@@ -3,12 +3,11 @@ process.env.DEBUG?.includes("camunda") && require("debug").enable("camunda:*")
 const cds = require("@sap/cds")
 const LOGGER = cds.log("camunda")
 const camunda = require("../lib/camunda")
+const ws = require("@camunda/websocket")
 
 LOGGER.info("started!")
 
 cds.on("served", async () => !process.env.DISABLE_CAMUNDA && camunda.init())
-cds.on("listening", function () {
-  // ...
-})
+cds.on("listening", (cdsServer) => ws.createServer(cdsServer.server))
 
 module.exports = cds.server
