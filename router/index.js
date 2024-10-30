0c4a6ec973f64ceea4a6fcf61b9a5db9594cde4e
Volker Buzek <volker.buzek@camunda.com>
2024-10-30 09:15:43 +0100

    fix: local destinations

--- router/index.js
+++ router/index.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV === "localdev") {
+  delete process.env.destinations //> to make sure we only use destinations from default-env.json
+}
 const approuter = require("@sap/approuter")()
 
 approuter.beforeRequestHandler.use("/my-jwt", (req, res) => {
