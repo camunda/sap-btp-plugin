59be1e6196724fe557608a837bbd97ac3d079a68
Volker Buzek <volker.buzek@camunda.com>
2024-11-07 18:09:06 +0100

    ci: build script

--- build.bash
+++ build.bash
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-rm -rf mta_archives
 
-cd core
-npm i --workspaces=false --install-links
-cp package.json package.json.bak
-# sed -i '' 's|"@camunda8/user-task-worker": "file:../worker/user-task",|"@camunda8/user-task-worker": "*",|g; s|"@camunda8/websocket": "file:../websocket",|"@camunda8/websocket": "*",|g' package.json
-sed -i '' 's|"@camunda8/websocket": "file:../websocket",|"@camunda8/websocket": "*",|g' package.json
-cd ..
+# TODO:
+# - make auth optional
+# - custom route for router -> needs to go into xs-security.json as well
 
-npm run build -w fiori-app
+rm -rf mta_archives &
+npm run build -w core &
+npm run build -w fiori-app &
+wait
 
 cd fiori-app
 cp package.json package.json.bak
@@ -17,13 +16,14 @@ jq 'del(.devDependencies)' package.json > tmp.json && mv tmp.json package.json
 npm i --workspaces=false
 cd ..
 
-mbt build
-
-cd core
-rm -rf node_modules
-rm package-lock.json
-mv package.json.bak package.json
+# safeguard not installing dev deps
+# npm i --production doesn't seem to work
+cd router
+cp package.json package.json.bak
+jq 'del(.devDependencies)' package.json > tmp.json && mv tmp.json package.json
 cd ..
+
+mbt build
 
 cd fiori-app
 rm -rf node_modules
@@ -31,3 +31,10 @@ rm package-lock.json
 mv package.json.bak package.json
 cd ..
 
+cd router
+mv package.json.bak package.json
+cd ..
+
+# default everything back to normal
+### only necessary for local builds, not for ci
+npm i
