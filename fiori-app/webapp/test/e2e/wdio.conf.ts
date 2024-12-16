35fd06c6fafafa2eaf16c9b88624e296d43d2fab
Volker Buzek <volker.buzek@camunda.com>
2024-12-16 14:59:00 +0100

    test(wdi5): increase timeout again again

--- fiori-app/webapp/test/e2e/wdio.conf.ts
+++ fiori-app/webapp/test/e2e/wdio.conf.ts
@@ -144,7 +144,7 @@ export const config: wdi5Config = {
     baseUrl: "http://localhost:8080/mockserver.html?channelId=testChannel",
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 30000,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
