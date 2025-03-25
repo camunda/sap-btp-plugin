ca8fb04fc1e2c5c40c4e19a273fdd076f754b3c0
Volker Buzek <volker.buzek@camunda.com>
2025-03-25 10:04:33 +0100

    feat(visibility,htmlview): adjust visibility in line w/ camunda prop

--- fiori-app/webapp/test/e2e/htmlview.test.ts
+++ fiori-app/webapp/test/e2e/htmlview.test.ts
@@ -42,10 +42,12 @@ describe("HTML control", () => {
   })
 
   it("should render FEEL/dynamically assigned HTML content correctly", async () => {
-    const feelVars = [{
-      name: "dynamicHeader",
-      value: "this is a dynamic header"
-    }]
+    const feelVars = [
+      {
+        name: "dynamicHeader",
+        value: "this is a dynamic header"
+      }
+    ]
     await injectFEEL("__xmlview0--BPMNform", feelVars)
 
     const pageSelector = {
@@ -72,11 +74,12 @@ describe("HTML control", () => {
   })
 
   it("should handle code injection attempts properly", async () => {
-
-    const feelVars = [{
-      name: "dynamicHeader",
-      value: "<script>alert('XSS')</script>"
-    }]
+    const feelVars = [
+      {
+        name: "dynamicHeader",
+        value: "<script>alert('XSS')</script>"
+      }
+    ]
     await injectFEEL("__xmlview0--BPMNform", feelVars)
 
     const pageSelector = {
@@ -102,5 +105,59 @@ describe("HTML control", () => {
     const containsExpected = innerHTML.some((html) => expected.test(html))
 
     expect(containsExpected).toBeTruthy()
+  })
+  
+  it("should hide HTML content when visibility set to false via FEEL", async () => {
+    const htmlSelector = {
+      selector: {
+        controlType: "sap.ui.core.HTML",
+        viewName: `${ns}.view.App`
+      },
+      forceSelect: true
+    }
+
+    const _all = await browser.allControls<HTML>(htmlSelector)
+    const all = []
+    for (const control of _all) {
+      all.push(await control.getWebElement())
+    }
+    // the UI5 html control is represented with "just a div"
+    // const divs = await html.$$("div")
+    const divs = []
+    for (const control of all) {
+      const controlDivs = await control.$$("div")
+      for (const div of controlDivs) {
+        divs.push(div)
+      }
+    }
+    const innerHTML = []
+    for (const div of divs) {
+      innerHTML.push(await div.getHTML(false)) // false to get inner HTML only
+    }
+    const expected = /<h3[^>]*>visibility<\/h3>/i //> from the model/design time
+    const containsExpected = innerHTML.some((html) => expected.test(html))
+    expect(containsExpected).toBeTruthy()
+
+    const feelVars = [{ name: "invisible", value: true }]
+    await injectFEEL("__xmlview0--BPMNform", feelVars)
+
+    const _allAfter = await browser.allControls<HTML>(htmlSelector)
+    const allAfter = []
+    for (const control of _allAfter) {
+      allAfter.push(await control.getWebElement())
+    }
+    const divsAfter = []
+    for (const control of allAfter) {
+      const controlDivs = await control.$$("div")
+      for (const div of controlDivs) {
+        divsAfter.push(div)
+      }
+    }
+    const innerHTMLAfter = []
+    for (const div of divsAfter) {
+      innerHTMLAfter.push(await div.getHTML(false))
+    }
+    const containsExpectedAfter = innerHTMLAfter.some((html) => expected.test(html))
+    expect(containsExpectedAfter).toBeFalsy()
   })
 })
