846b367ea994a1ac785983b7eb4b3bcdbdda1c5b
Volker Buzek <volker.buzek@camunda.com>
2025-03-26 23:43:09 +0100

    ci: longer pause for ui5 tooling modules...

--- fiori-app/webapp/test/e2e/1_dummy.test.ts
+++ fiori-app/webapp/test/e2e/1_dummy.test.ts
@@ -8,22 +8,23 @@ describe("dummy", () => {
     await browser.goTo(mockIndex())
     await ui5Service.injectUI5()
     await formTarget("checkbox-8.6")
-    await browser.screenshot("before-dummy")
-  })
-
-  beforeEach(async () => {
-    await browser.screenshot("before-each-dummy")
-  })
-
-  afterEach(async () => {
-    const pause = 3000
+    const pause = process.env.ci ? 6000 : 1000
     wdi5.getLogger().info(`>>>>>>>>>> pausing for ${pause} ms to let ui5-tooling-modules do its work...`)
     await browser.pause(pause)
     wdi5.getLogger().info(">>>>>>>>>> ...done pausing - let's do this!")
-    await browser.screenshot("after-each-dummy")
+    await browser.screenshot("before-dummy")
   })
 
-  it("basic select/deselect", async () => {
+  // beforeEach(async () => {
+  //   await browser.screenshot("before-each-dummy")
+  // })
+
+  // afterEach(async () => {
+
+  //   await browser.screenshot("after-each-dummy")
+  // })
+
+  it("foo", async () => {
     expect(true).toBeTruthy()
   })
 })
