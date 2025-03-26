25becda1b1f338b07e7e1618d31c84556a722892
Volker Buzek <volker.buzek@camunda.com>
2025-03-26 21:55:20 +0100

    chore: reformat

--- user-task-fiori/src/creations/addTextArea.ts
+++ user-task-fiori/src/creations/addTextArea.ts
@@ -1,22 +1,22 @@
-import TextArea from "sap/m/TextArea";
-import Control from "sap/ui/core/Control";
-import { Component, ControlType } from "../BPMNformData";
-import { evaluate } from "feelers";
-import BPMNForm from "../BPMNForm";
+import TextArea from "sap/m/TextArea"
+import Control from "sap/ui/core/Control"
+import { Component, ControlType } from "../BPMNformData"
+import { evaluate } from "feelers"
+import BPMNForm from "../BPMNForm"
 
 export function addTextArea(this: BPMNForm, element: Component): Control {
   const defaultValue: string =
     this.getLocalModel().getProperty(`/BPMNform/${element.key}`) ||
     this.getLocalModel().getProperty(`/BPMNform/variables/${element.key}`) ||
-    element.defaultValue;
+    element.defaultValue
 
-  const enabled = element.disabled;
+  const enabled = element.disabled
   const readonly = element.readonly
     ? // eslint-disable-next-line @typescript-eslint/no-unsafe-call
       !!evaluate(element.readonly.toString(), this.getLocalModel().getProperty("/BPMNform/variables"))
-    : false;
-  const required = element.validate?.required || false;
-
+    : false
+  const required = element.validate?.required || false
+  
   const control = new TextArea(this.generateControlId(element), {
     visible: this.getVisibleStatement(element),
     value: defaultValue,
@@ -26,13 +26,13 @@ export function addTextArea(this: BPMNForm, element: Component): Control {
     cols: 50,
     rows: 20,
     liveChange: (event) => {
-      this.provideValueToView(element, control);
-      this.setValueState(control, element, event.getParameter("value"));
+      this.provideValueToView(element, control)
+      this.setValueState(control, element, event.getParameter("value"))
     }
-  });
+  })
 
-  this.addControl(element, control, ControlType.Textarea);
-  this.setValueState(control, element, control.getValue());
+  this.addControl(element, control, ControlType.Textarea)
+  this.setValueState(control, element, control.getValue())
 
-  return control;
+  return control
 }
