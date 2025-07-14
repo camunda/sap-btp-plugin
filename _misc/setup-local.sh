d54c5b676dd5169026ff76e33586ca5c2514e9cc
Volker Buzek <volker.buzek@camunda.com>
2025-07-14 16:00:41 +0200

    refactor: remove symbolic link creation for .cdsrc-private.json in core folder

--- _misc/setup-local.sh
+++ _misc/setup-local.sh
@@ -11,10 +11,5 @@ done
 # make the @sap/approuter aware of traffic dispatching locally
 cp "_misc/default-env.json" "./router"
 
-# link the .cdsrc-private.json to the core folder
-if [ ! -L "./core/.cdsrc-private.json" ]; then
-  ln -s .cdsrc-private.json ./core
-fi
-
 # deploy sample data
 cds deploy --profile hybrid
