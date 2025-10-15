90c80201d5f3ff792e86491fc697da4b9d3ead25
Constantin Lebrecht <constantin@monavari.de>
2025-10-15 11:41:40 +0200

    feat: add e2e test for starting process via url (#43)

--- fiori-app/webapp/test/playwrightE2E/basic.spec.ts
+++ fiori-app/webapp/test/playwrightE2E/basic.spec.ts
@@ -36,7 +36,10 @@ async function fetchProcessInstances(processDefinitionId: string): Promise<any> 
 
 test('Start process via UI and check instances on camunda', async ({ page }) => {
   await page.goto('/')
+
+  // fetch instances before starting a new one
   const instancesBefore = await fetchProcessInstances('e2eTestProcess');
+
   await page.getByRole('button', { name: 'menu2' }).click();
   await page.getByRole('menuitem', { name: 'run this process...' }).click();
   await page.locator('[id="__component0---app--processName-inner"]').fill('e2eTestProcess');
@@ -47,6 +50,27 @@ test('Start process via UI and check instances on camunda', async ({ page }) => 
   await expect(page.getByRole('button', { name: 'Next' })).toBeVisible();
   await page.getByRole('button', { name: 'Next' }).click();
 
+ // Polling until the process instance count increases by 1. This is necessary because starting a process might take some time.
+  await expect.poll(async () => {
+    return fetchProcessInstances('e2eTestProcess');
+  }, {
+    message: `Process instance should be increased to ${instancesBefore + 1}`,
+    timeout: 10000 // timeout after 10 sec
+  }).toBe(instancesBefore + 1); 
+})
+
+test.only('Start process via URL and check instances on camunda', async ({ page }) => {
+  await page.goto('?run=e2eTestProcess')
+  
+  // fetch instances before starting a new one
+  const instancesBefore = await fetchProcessInstances('e2eTestProcess');
+
+  await expect(page.getByText('Success! This form is delivered by Camunda.')).toBeVisible();
+
+  await expect(page.getByRole('button', { name: 'Next' })).toBeVisible();
+  await page.getByRole('button', { name: 'Next' }).click();
+
+  // Polling until the process instance count increases by 1. This is necessary because starting a process might take some time.
   await expect.poll(async () => {
     return fetchProcessInstances('e2eTestProcess');
   }, {
