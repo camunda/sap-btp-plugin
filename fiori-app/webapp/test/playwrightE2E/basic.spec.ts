f9b7fa8250d81d207c279d99be39388ebbc23377
Constantin Lebrecht <constantin@monavari.de>
2025-10-15 12:25:44 +0200

    feat: refactor test to check for final-user-task

--- fiori-app/webapp/test/playwrightE2E/basic.spec.ts
+++ fiori-app/webapp/test/playwrightE2E/basic.spec.ts
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from "@playwright/test"
 
-test('App loads and shows title', async ({ page }) => {
-  await page.goto('/') // nutzt baseURL
+test("App loads and shows title", async ({ page }) => {
+  await page.goto("/") // nutzt baseURL
   await expect(page).toHaveTitle(/Camunda SAP BTP Integration/i)
 })
 
@@ -10,71 +10,125 @@ test('App loads and shows title', async ({ page }) => {
  * @param processDefinitionId ID if process
  * @returns count of process instances
  */
-async function fetchProcessInstances(processDefinitionId: string): Promise<any> {
+async function fetchProcessInstances(
+  processDefinitionId: string
+): Promise<{ page: { totalItems: number }; items: { processInstanceKey: string; state: string }[] }> {
   return new Promise((resolve, reject) => {
-    fetch('http://localhost:8088/v2/process-instances/search', { // Ihre Anforderung
-      method: 'POST',
+    fetch("http://localhost:8088/v2/process-instances/search", {
+      // Ihre Anforderung
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest',
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest"
       },
       body: JSON.stringify({
         filter: {
-          processDefinitionId: processDefinitionId 
-        }
+          processDefinitionId: processDefinitionId
+        },
+        sort: [{ field: "startDate", order: "DESC" }]
       })
     })
-      .then(response => response.json())
-      .then(data => {
-        resolve(data.page.totalItems); // count of instances
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(data) // count of instances
       })
-      .catch(error => {
-        reject(error);
-      });
+      .catch((error) => {
+        reject(error)
+      })
   })
 }
 
-test('Start process via UI and check instances on camunda', async ({ page }) => {
-  await page.goto('/')
+async function fetchProcessInstanceById(
+  processInstanceId: string
+): Promise<{ processInstanceKey: string; state: string }> {
+  return new Promise((resolve, reject) => {
+    fetch(`http://localhost:8088/v2/process-instances/${processInstanceId}`, {
+      // Ihre Anforderung
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest"
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(data) // count of instances
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+test("Start process via UI and check instances on camunda", async ({ page }) => {
+  await page.goto("/")
 
   // fetch instances before starting a new one
-  const instancesBefore = await fetchProcessInstances('e2eTestProcess');
+  const instancesBefore = (await fetchProcessInstances("e2eTestProcess")).page.totalItems
 
-  await page.getByRole('button', { name: 'menu2' }).click();
-  await page.getByRole('menuitem', { name: 'run this process...' }).click();
-  await page.locator('[id="__component0---app--processName-inner"]').fill('e2eTestProcess');
-  await page.getByRole('button', { name: 'start above process' }).click();
+  await page.getByRole("button", { name: "menu2" }).click()
+  await page.getByRole("menuitem", { name: "run this process..." }).click()
+  await page.locator('[id="__component0---app--processName-inner"]').fill("e2eTestProcess")
+  await page.getByRole("button", { name: "start above process" }).click()
 
-  await expect(page.getByText('Success! This form is delivered by Camunda.')).toBeVisible();
-  
-  await expect(page.getByRole('button', { name: 'Next' })).toBeVisible();
-  await page.getByRole('button', { name: 'Next' }).click();
+  await expect(page.getByText("Success! This form is delivered by Camunda.")).toBeVisible()
 
- // Polling until the process instance count increases by 1. This is necessary because starting a process might take some time.
-  await expect.poll(async () => {
-    return fetchProcessInstances('e2eTestProcess');
-  }, {
-    message: `Process instance should be increased to ${instancesBefore + 1}`,
-    timeout: 10000 // timeout after 10 sec
-  }).toBe(instancesBefore + 1); 
-})
-
-test('Start process via URL and check instances on camunda', async ({ page }) => {
-  await page.goto('?run=e2eTestProcess')
-  
-  // fetch instances before starting a new one
-  const instancesBefore = await fetchProcessInstances('e2eTestProcess');
-
-  await expect(page.getByText('Success! This form is delivered by Camunda.')).toBeVisible();
-
-  await expect(page.getByRole('button', { name: 'Next' })).toBeVisible();
-  await page.getByRole('button', { name: 'Next' }).click();
+  await expect(page.getByRole("button", { name: "Next" })).toBeVisible()
+  await page.getByRole("button", { name: "Next" }).click()
 
   // Polling until the process instance count increases by 1. This is necessary because starting a process might take some time.
-  await expect.poll(async () => {
-    return fetchProcessInstances('e2eTestProcess');
-  }, {
-    message: `Process instance should be increased to ${instancesBefore + 1}`,
-    timeout: 10000 // timeout after 10 sec
-  }).toBe(instancesBefore + 1); 
+  await expect
+    .poll(
+      async () => {
+        return (await fetchProcessInstances("e2eTestProcess")).page.totalItems
+      },
+      {
+        message: `Process instance should be increased to ${instancesBefore + 1}`,
+        timeout: 10000 // timeout after 10 sec
+      }
+    )
+    .toBe(instancesBefore + 1)
+})
+
+test("Start process via URL and check instances on camunda", async ({ page }) => {
+  let currentInstance: { processInstanceKey: string; state: string }
+  await page.goto("?run=e2eTestProcess")
+
+  // fetch instances before starting a new one
+  const instances = await fetchProcessInstances("e2eTestProcess")
+  const instancesBefore = instances.page.totalItems
+
+  await expect(page.getByText("Success! This form is delivered by Camunda.")).toBeVisible()
+
+  await expect(page.getByRole("button", { name: "Next" })).toBeVisible()
+  await page.getByRole("button", { name: "Next" }).click()
+
+  // Polling until the process instance count increases by 1. This is necessary because starting a process might take some time.
+  await expect
+    .poll(
+      async () => {
+        const instances = await fetchProcessInstances("e2eTestProcess")
+        currentInstance = instances.items[0] as { processInstanceKey: string; state: string }
+        return instances.page.totalItems
+      },
+      {
+        message: `Process instance should be increased to ${instancesBefore + 1}`,
+        timeout: 10000 // timeout after 10 sec
+      }
+    )
+    .toBe(instancesBefore + 1)
+
+  // Polling current process until its state is COMPLETED
+  await expect
+    .poll(
+      async () => {
+        const instance = await fetchProcessInstanceById(currentInstance.processInstanceKey)
+        return instance.state
+      },
+      {
+        message: `Process should be COMPLETED`,
+        timeout: 10000 // timeout after 10 sec
+      }
+    )
+    .toBe("COMPLETED")
 })
